5836593516ac8e7c5ebaefb29ee76cad26c55ce8
Tarun Bhavnani <39047647+tarunbhavnani@users.noreply.github.com>
2018-12-24 15:05:45 +0530

    Update sigmoid-softmax.py

--- sigmoid-softmax.py
+++ sigmoid-softmax.py
@@ -27,3 +27,13 @@ def sm(x):
   return(ex/ex.sum(axis=0))
 sm(x)
 sm(x).sum()
+
+#bonus: cosine similarity
+def cos_sim(a, b):
+	"""Takes 2 vectors a, b and returns the cosine similarity according 
+	to the definition of the dot product
+	"""
+	dot_product = np.dot(a, b)
+	norm_a = np.linalg.norm(a)
+	norm_b = np.linalg.norm(b)
+	return dot_product / (norm_a * norm_b)
