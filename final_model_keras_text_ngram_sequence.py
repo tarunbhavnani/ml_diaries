02efdc401a6be678687ae572ce58dfeec1b3b15e
Tarun Bhavnani <39047647+tarunbhavnani@users.noreply.github.com>
2019-01-14 16:39:14 +0530

    Add files via upload

---
+++ final_model_keras_text_ngram_sequence.py
@@ -0,0 +1,528 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 10:30:40 2019
+
+@author: tarun.bhavnani@dev.smecorner.com
+"""
+#https://developers.google.com/machine-learning/guides/text-classification/step-1
+#amazing tut
+import os
+import numpy as np
+import pandas as pd
+import random
+os.chdir("/home/tarun.bhavnani@dev.smecorner.com/Desktop/keras")
+
+
+def load_imdb_sentiment_analysis_dataset(data_path, seed=123):
+    """Loads the IMDb movie reviews sentiment analysis dataset.
+
+    # Arguments
+        data_path: string, path to the data directory.
+        seed: int, seed for randomizer.
+
+    # Returns
+        A tuple of training and validation data.
+        Number of training samples: 25000
+        Number of test samples: 25000
+        Number of categories: 2 (0 - negative, 1 - positive)
+
+    # References
+        Mass et al., http://www.aclweb.org/anthology/P11-1015
+
+        Download and uncompress archive from:
+        http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+    """
+    imdb_data_path = os.path.join(os.getcwd(), 'aclImdb')
+
+    # Load the training data
+    train_texts = []
+    train_labels = []
+    for category in ['pos', 'neg']:
+        train_path = os.path.join(imdb_data_path, 'train', category)
+        for fname in sorted(os.listdir(train_path)):
+            if fname.endswith('.txt'):
+                with open(os.path.join(train_path, fname)) as f:
+                    train_texts.append(f.read())
+                train_labels.append(0 if category == 'neg' else 1)
+
+    # Load the validation data.
+    test_texts = []
+    test_labels = []
+    for category in ['pos', 'neg']:
+        test_path = os.path.join(imdb_data_path, 'test', category)
+        for fname in sorted(os.listdir(test_path)):
+            if fname.endswith('.txt'):
+                with open(os.path.join(test_path, fname)) as f:
+                    test_texts.append(f.read())
+                test_labels.append(0 if category == 'neg' else 1)
+
+    # Shuffle the training data and labels.
+    random.seed(seed)
+    random.shuffle(train_texts)
+    random.seed(seed)
+    random.shuffle(train_labels)
+
+    return ((train_texts, np.array(train_labels)),
+            (test_texts, np.array(test_labels)))
+
+path= os.getcwd()
+(train_texts, train_labels), (test_texts, test_labels)=load_imdb_sentiment_analysis_dataset(path)
+
+from collections import Counter
+Counter(train_labels)
+Counter(test_labels)
+
+
+#import numpy as np
+import matplotlib.pyplot as plt
+
+def get_num_words_per_sample(sample_texts):
+    """Returns the median number of words per sample given corpus.
+
+    # Arguments
+        sample_texts: list, sample texts.
+
+    # Returns
+        int, median number of words per sample.
+    """
+    num_words = [len(s.split()) for s in sample_texts]
+    return np.median(num_words)
+
+
+get_num_words_per_sample(train_texts)
+get_num_words_per_sample(test_texts)
+
+def plot_sample_length_distribution(sample_texts):
+    """Plots the sample length distribution.
+
+    # Arguments
+        samples_texts: list, sample texts.
+    """
+    plt.hist([len(s) for s in sample_texts], 50)
+    plt.xlabel('Length of a sample')
+    plt.ylabel('Number of samples')
+    plt.title('Sample length distribution')
+    plt.show()
+plot_sample_length_distribution(train_texts)
+plot_sample_length_distribution(test_texts)
+
+
+"""
+#1)“How do we present the text data to an algorithm that expects numeric input?” 
+(this is called data preprocessing and vectorization), 
+#2)“What type of model should we use?”, 
+#3)“What configuration parameters should we use for our model?”, etc.
+"""
+
+#################333
+#1) N-Gram approach
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+
+# Vectorization parameters
+# Range (inclusive) of n-gram sizes for tokenizing text.
+NGRAM_RANGE = (1, 2)
+
+# Limit on the number of features. We use the top 20K features.
+TOP_K = 200# its 20000
+
+
+# Whether text should be split into word or character n-grams.
+# One of 'word', 'char'.
+TOKEN_MODE = 'word'
+
+# Minimum document/corpus frequency below which a token will be discarded.
+MIN_DOCUMENT_FREQUENCY = 2
+
+val_texts=test_texts
+
+def ngram_vectorize(train_texts, train_labels, val_texts):
+    """Vectorizes texts as n-gram vectors.
+
+    1 text = 1 tf-idf vector the length of vocabulary of unigrams + bigrams.
+
+    # Arguments
+        train_texts: list, training text strings.
+        train_labels: np.ndarray, training labels.
+        val_texts: list, validation text strings.
+
+    # Returns
+        x_train, x_val: vectorized training and validation texts
+    """
+    # Create keyword arguments to pass to the 'tf-idf' vectorizer.
+    kwargs = {
+            'ngram_range': NGRAM_RANGE,  # Use 1-grams + 2-grams.
+            'dtype': 'int32',
+            'strip_accents': 'unicode',
+            'decode_error': 'replace',
+            'analyzer': TOKEN_MODE,  # Split text into word tokens.
+            'min_df': MIN_DOCUMENT_FREQUENCY,
+    }
+    vectorizer = TfidfVectorizer(**kwargs)
+
+    # Learn vocabulary from training texts and vectorize training texts.
+    x_train = vectorizer.fit_transform(train_texts)
+
+    # Vectorize validation texts.
+    x_val = vectorizer.transform(val_texts)
+
+    # Select top 'k' of the vectorized features.
+    selector = SelectKBest(f_classif, k=min(TOP_K, x_train.shape[1]))
+    selector.fit(x_train, train_labels)
+    x_train = selector.transform(x_train).astype('float32')
+    x_val = selector.transform(x_val).astype('float32')
+    return x_train, x_val
+
+x_train_ng,x_test_ng=ngram_vectorize(train_texts, train_labels, val_texts)
+#xt,xv= xt.toarray(), xv.toarray()
+"""
+#in this we loose the order of words and grammer.
+#models that don’t take ordering into account, such as logistic regression, 
+multi-layer perceptrons, gradient boosting machines, support vector machines.
+"""
+
+
+#2) Sequence Vectors
+#this we use when we have to follow the oreder of words!!
+
+#word models are better, character is good when there are lot of typos!
+
+#see
+"""
+text="the mouse ran up the clock"
+w2idx={'clock': 5, 'ran': 3, 'up': 4, 'down': 6, 'the': 1, 'mouse': 2}
+seq=[w2idx[i] for i in text.split()]
+
+#this can be proceeded with two approached:
+"#1) One Hot Encoding"
+a= np.zeros((len(text.split()), len(w2idx)))
+for i, j in enumerate(text.split()):
+  print(i,j)
+  a[i,w2idx[j]]=1
+
+
+#2) Word Embeddings"
+each word index gets mapped to a dense vector of real values representing 
+that word’s location in semantic space "
+"""
+
+from tensorflow.python.keras.preprocessing import sequence
+from tensorflow.python.keras.preprocessing import text
+from keras.preprocessing.text import Tokenizer
+
+
+# Vectorization parameters
+# Limit on the number of features. We use the top 20K features.
+TOP_K = 200 #20000
+
+# Limit on the length of text sequences. Sequences longer than this
+# will be truncated.
+MAX_SEQUENCE_LENGTH = 500
+
+def sequence_vectorize(train_texts, val_texts):
+    """Vectorizes texts as sequence vectors.
+
+    1 text = 1 sequence vector with fixed length.
+
+    # Arguments
+        train_texts: list, training text strings.
+        val_texts: list, validation text strings.
+
+    # Returns
+        x_train, x_val, word_index: vectorized training and validation
+            texts and word index dictionary.
+    """
+    # Create vocabulary with training texts.
+    tokenizer = Tokenizer(num_words=TOP_K)
+    tokenizer.fit_on_texts(train_texts)
+
+    # Vectorize training and validation texts.
+    x_train = tokenizer.texts_to_sequences(train_texts)
+    x_val = tokenizer.texts_to_sequences(val_texts)
+
+    # Get max sequence length.
+    max_length = len(max(x_train, key=len))
+    if max_length > MAX_SEQUENCE_LENGTH:
+        max_length = MAX_SEQUENCE_LENGTH
+
+    # Fix sequence length to max value. Sequences shorter than the length are
+    # padded in the beginning and sequences longer are truncated
+    # at the beginning.
+    x_train = sequence.pad_sequences(x_train, maxlen=max_length)
+    x_val = sequence.pad_sequences(x_val, maxlen=max_length)
+    return x_train, x_val, tokenizer.word_index
+
+X_train, X_test, w2idx=sequence_vectorize(train_texts, test_texts)
+
+
+# now we can sit back for a while and analyze the diff data we have received from both the approaches
+#n-gram gives us:
+x_train_ng.shape, x_test_ng.shape
+#((25000, 200), (25000, 200))
+#25000 documents with 200 words
+X_train.shape, X_test.shape, len(w2idx)
+#((25000, 500), (25000, 500), 88582)
+#25000 documents, with msl of 500. and total words in data=88582
+#but only 200 top words used in data, can view X_train, each
+#line is a sequence of numbers<200.
+
+
+#get last layer
+def _get_last_layer_units_and_activation(num_classes):
+    """Gets the # units and activation function for the last network layer.
+
+    # Arguments
+        num_classes: int, number of classes.
+
+    # Returns
+        units, activation values.
+    """
+    if num_classes == 2:
+        activation = 'sigmoid'
+        units = 1
+    else:
+        activation = 'softmax'
+        units = num_classes
+    return units, activation
+
+
+#n gram model
+    
+from tensorflow.python.keras import models
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+def mlp_model(layers, units, dropout_rate, input_shape, num_classes):
+    """Creates an instance of a multi-layer perceptron model.
+
+    # Arguments
+        layers: int, number of `Dense` layers in the model.
+        units: int, output dimension of the layers.
+        dropout_rate: float, percentage of input to drop at Dropout layers.
+        input_shape: tuple, shape of input to the model.
+        num_classes: int, number of output classes.
+
+    # Returns
+        An MLP model instance.
+    """
+    op_units, op_activation = _get_last_layer_units_and_activation(num_classes)
+    model = Sequential()
+    model.add(Dropout(rate=dropout_rate, input_shape=input_shape))
+
+    for _ in range(layers-1):
+        model.add(Dense(units=units, activation='relu'))
+        model.add(Dropout(rate=dropout_rate))
+
+    model.add(Dense(units=op_units, activation=op_activation))
+    # model.summary()
+    return model
+  
+#check
+md=mlp_model(layers=3, units=100, dropout_rate=.1, input_shape=(6,6),num_classes=2)
+md.summary()
+
+
+
+
+
+
+# Sequence Model
+
+from tensorflow.python.keras import models
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from tensorflow.python.keras.layers import Embedding
+from tensorflow.python.keras.layers import SeparableConv1D
+from tensorflow.python.keras.layers import MaxPooling1D
+from tensorflow.python.keras.layers import GlobalAveragePooling1D
+
+def sepcnn_model(blocks,
+                 filters,
+                 kernel_size,
+                 embedding_dim,
+                 dropout_rate,
+                 pool_size,
+                 input_shape,
+                 num_classes,
+                 num_features,
+                 use_pretrained_embedding=False,
+                 is_embedding_trainable=False,
+                 embedding_matrix=None):
+    """Creates an instance of a separable CNN model.
+
+    # Arguments
+        blocks: int, number of pairs of sepCNN and pooling blocks in the model.
+        filters: int, output dimension of the layers.
+        kernel_size: int, length of the convolution window.
+        embedding_dim: int, dimension of the embedding vectors.
+        dropout_rate: float, percentage of input to drop at Dropout layers.
+        pool_size: int, factor by which to downscale input at MaxPooling layer.
+        input_shape: tuple, shape of input to the model.
+        num_classes: int, number of output classes.
+        num_features: int, number of words (embedding input dimension).
+        use_pretrained_embedding: bool, true if pre-trained embedding is on.
+        is_embedding_trainable: bool, true if embedding layer is trainable.
+        embedding_matrix: dict, dictionary with embedding coefficients.
+
+    # Returns
+        A sepCNN model instance.
+    """
+    op_units, op_activation = _get_last_layer_units_and_activation(num_classes)
+    model = models.Sequential()
+
+    # Add embedding layer. If pre-trained embedding is used add weights to the
+    # embeddings layer and set trainable to input is_embedding_trainable flag.
+    if use_pretrained_embedding:
+        model.add(Embedding(input_dim=num_features,
+                            output_dim=embedding_dim,
+                            input_length=input_shape[0],
+                            weights=[embedding_matrix],
+                            trainable=is_embedding_trainable))
+    else:
+        model.add(Embedding(input_dim=num_features,
+                            output_dim=embedding_dim,
+                            input_length=input_shape[0]))
+
+    for _ in range(blocks-1):
+        model.add(Dropout(rate=dropout_rate))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(MaxPooling1D(pool_size=pool_size))
+
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Dense(op_units, activation=op_activation))
+    return model
+
+
+
+
+def train_ngram_model(data,
+                      learning_rate=1e-3,
+                      epochs=1000,
+                      batch_size=128,
+                      layers=2,
+                      units=64,
+                      dropout_rate=0.2):
+    """Trains n-gram model on the given dataset.
+
+    # Arguments
+        data: tuples of training and test texts and labels.
+        learning_rate: float, learning rate for training model.
+        epochs: int, number of epochs.
+        batch_size: int, number of samples per batch.
+        layers: int, number of `Dense` layers in the model.
+        units: int, output dimension of Dense layers in the model.
+        dropout_rate: float: percentage of input to drop at Dropout layers.
+
+    # Raises
+        ValueError: If validation data has label values which were not seen
+            in the training data.
+    """
+    # Get the data.
+    (train_texts, train_labels), (val_texts, val_labels) = data
+
+    # Verify that validation labels are in the same range as training labels.
+    num_classes = explore_data.get_num_classes(train_labels)
+    unexpected_labels = [v for v in val_labels if v not in range(num_classes)]
+    if len(unexpected_labels):
+        raise ValueError('Unexpected label values found in the validation set:'
+                         ' {unexpected_labels}. Please make sure that the '
+                         'labels in the validation set are in the same range '
+                         'as training labels.'.format(
+                             unexpected_labels=unexpected_labels))
+
+    # Vectorize texts.
+    x_train, x_val = vectorize_data.ngram_vectorize(
+        train_texts, train_labels, val_texts)
+
+    # Create model instance.
+    model = build_model.mlp_model(layers=layers,
+                                  units=units,
+                                  dropout_rate=dropout_rate,
+                                  input_shape=x_train.shape[1:],
+                                  num_classes=num_classes)
+
+    # Compile model with learning parameters.
+    if num_classes == 2:
+        loss = 'binary_crossentropy'
+    else:
+        loss = 'sparse_categorical_crossentropy'
+    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
+
+    # Create callback for early stopping on validation loss. If the loss does
+    # not decrease in two consecutive tries, stop training.
+    callbacks = [tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=2)]
+
+    # Train and validate model.
+    history = model.fit(
+            x_train,
+            train_labels,
+            epochs=epochs,
+            callbacks=callbacks,
+            validation_data=(x_val, val_labels),
+            verbose=2,  # Logs once per epoch.
+            batch_size=batch_size)
+
+    # Print results.
+    history = history.history
+    print('Validation accuracy: {acc}, loss: {loss}'.format(
+            acc=history['val_acc'][-1], loss=history['val_loss'][-1]))
+
+    # Save model.
+    model.save('IMDb_mlp_model.h5')
+    return history['val_acc'][-1], history['val_loss'][-1]
+  
+  
+
+
+
+#imp Hyperparameters:
+  #Number of layers in the model: too many can cause overfitting,for 
+  #text: 2-3 layer mlp or 4 layer sepCNN
+  #Number of units per layer: 32/64
+  # Dropout: Recommended range: 0.2–0.5.
+  # Learning Rate
+  #If your model is not learning, try decreasing learning rate.
+  #Kernel size: The size of the convolution window. Recommended values: 3 or 5.
+  # Embedding dimensions: The number of dimensions we want to use to represent 
+  #word embeddings—i.e., the size of each word vector. Recommended values: 50–300.
+
+
+
+
+
+
+
+
