953dad4449e585f6012f5aafe2eb047a32cd3054
tarun <tarun.bhavnani@gmail.com>
2021-04-08 13:19:34 +0530

    watermark remover flask 2

--- flask/watermark-remover/app.py
+++ flask/watermark-remover/app.py
@@ -59,6 +59,10 @@ def uploaded_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
