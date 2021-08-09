0c40e3f93cae22843974e74e998ade72dfd6ed2f
tarun <tarun.bhavnani@gmail.com>
2021-08-09 11:34:45 +0530

    reg_ind

--- da/file_processor.py
+++ da/file_processor.py
@@ -14,6 +14,23 @@ from docx import Document
 from pptx import Presentation
 
 
+def ngrams(string):
+    n=4
+    string = re.sub(r'[,-./]|\sBD', r'', string)
+    words=string.split()
+    ngram=[]
+    for word in words:
+        #print(word)
+        ngrams = zip(*[word[i:] for i in range(n)])
+        ret=[''.join(ngram) for ngram in ngrams]
+        [ngram.append(i) for i in ret]
+    #[ngram.append(i) for i in string.split()]
+    return ngram
+
+
+
+
+
 import glob
 files=glob.glob(r"C:\Users\ELECTROBOT\Desktop\da_test_files\*")
 
@@ -141,3 +158,111 @@ def files_processor_tb(files):
     
 
     return tb_index, all_sents, vec, tfidf_matrix
+
+
+tb_index, all_sents, vec, tfidf_matrix=files_processor_tb(files)
+
+
+
+
+question= "who did federer married"
+question_tfidf = " ".join([i for i in question.split() if i not in stopwords])
+question_vec = vec.transform([question_tfidf])
+
+
+
+scores = cosine_similarity(tfidf_matrix, question_vec)
+scores = [i[0] for i in scores]
+dict_scores = {i: j for i, j in enumerate(scores)}
+dict_scores = {k: v for k, v in sorted(dict_scores.items(), key=lambda item: item[1], reverse=True)}
+# get top n sentences
+# final_response_dict=[self.tb_index[i] for i in dict_scores]
+final_response_dict = [tb_index[i] for i, j in dict_scores.items() if j > 0.1]
+final_response_dict = [i for i in final_response_dict if len(i['sentence'].split()) > 7]
+
+
+
+
+
+# =============================================================================
+# regex from tb_index
+# =============================================================================
+
+# reg= r'roger|married'
+
+# reg=r'^(?=.*\bfederer\b)(?=.*\bmarried\b).*$'
+
+
+# def reg_ind(word, tb_index):
+#     #pass
+
+#     tb_index_reg=[len(re.findall(word,i['sentence'].lower())) for i in tb_index]
+#     #tb_index[0]['reg']=2
+
+#     for num,i in enumerate(tb_index_reg):
+#         tb_index[num]['reg']=i
+
+#     tb_index_reg= [i for i in tb_index if i['reg']>0]
+    
+#     docs= list(set([i['doc'] for i in tb_index_reg]))
+#     overall_dict={i:sum([j['reg'] for j in tb_index_reg if j['doc']==i]) for i in docs}
+    
+    
+    
+#     return tb_index_reg, overall_dict
+
+
+# jkl,jkl1= reg_ind("flask", tb_index)
+
+
+
+
+words= "federer , final, atp" #if any
+words="federer + final + atp"# plus if all
+words= "married "# nothing then search exact
+
+def reg_ind(words, tb_index):
+    if "," in words:
+        #words= words.split(',')
+        words= [i.strip() for i in words.split(",")]
+        reg= "|".join(words)
+        tb_index_reg=tb_index
+        tb_index_reg=[i for i in tb_index if len(re.findall(reg, i['sentence']))>0]
+        
+    elif "+" in words:
+        words= [i.strip() for i in words.split("+")]
+        tb_index_reg=tb_index
+        for word in words:   
+            tb_index_reg=[i for i in tb_index_reg if len(re.findall(word, i['sentence']))>0]
+    else:
+        words= words.strip()
+        tb_index_reg=[i for i in tb_index if len(re.findall(words, i['sentence']))>0]
+    
+    
+    docs= list(set([i['doc'] for i in tb_index_reg]))
+    
+    overall_dict={i:sum([1 for j in tb_index_reg if j['doc']==i]) for i in docs}
+    #number of sentences not occurances
+    
+    return tb_index_reg, overall_dict, docs
+    
+
+
+def extract_doc_reg_index(tb_index_reg, doc):
+    req_tb_index=[i for i in tb_index_reg if i['doc']==doc]
+    req_df=pd.DataFrame(req_tb_index)
+    req_df.drop('doc', axis=1, inplace=True)
+    #convert to html on put on webpage
+    return req_df
+    
+
+
+
+tb_index_reg, overall_dict, docs=reg_ind(words, tb_index)
+
+import pandas as pd
+rf_ind=extract_doc_reg_index(tb_index_reg, 'rf.pdf')
+
+        
+        
+    
