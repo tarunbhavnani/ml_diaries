8755ce85caa93325d08149942b500b72520df0ed
tarun <tarun.bhavnani@gmail.com>
2023-01-10 10:19:39 +0530

    unit tests

--- Fsatapi_sentiment/app/tests/routers/test_sentiment.py
+++ Fsatapi_sentiment/app/tests/routers/test_sentiment.py
@@ -2,14 +2,19 @@ import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import patch, Mock
 from app.main import app
+from app.routers import sentiment
 from app.functions import get_sentiment
 client = TestClient(app)
 
 
+
+
 def test_get_sentiment_scores():
+
     response= client.post("/sentiment", json={"text":"Good"})
+
     assert response.status_code == 200
-    assert response.json()["text"]=="Good"
+    assert response.json()["text"]== "Good"
 
 
 
