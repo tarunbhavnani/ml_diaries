ee8ef38cb07b37670d74106b736cfe31a1e4895d
tarunbhavnani <tarun.bhavnani@gmail.com>
2023-08-14 15:47:01 +0530

    main file

--- DA_fastapi_full/app/main.py
+++ DA_fastapi_full/app/main.py
@@ -17,16 +17,31 @@ else:
 	pass_context= jinja2.contextfunction
 
 
-app= FastAPI(root_path= os.environ.get("SCRIPT_NAME"), dependencies= [Depends(fetch_user)])
-app.include(predict_router)
+from app.logger import logger
+from .environment import ENVIRONMENT
+from app.dependencies import fetch_user
 
+from app.routers import redis, predict, uploadfiles, reset, filename_redis, transferfiles, collections_available, get_pdf
+
+
+app= FastAPI(root_path= os.environ.get("SCRIPT_NAME"), dependencies= [Depends(fetch_user)])
+app.include_router(redis.router)
+app.include_router(predict.router)
+app.include_router(uploadfiles.router)
+app.include_router(reset.router)
+app.include_router(filename_redis.router)
+app.include_router(transferfiles.router)
+app.include_router(collections_available.router)
+app.include_router(get_pdf.router)
 
 
 
 
 
 from fastapi.middleware.cors import CORSMiddleware
+
 origins=["https://127.0.0.1:8080", "https://localhost:8080"]
+
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=origins,
@@ -35,19 +50,24 @@ app.add_middleware(
 	allow_headers=["*"]
 	)
 
-BASE_DIR="./nlp-qna/app"
+ENVIRONMENT='LOCAL'
+
+if ENVIRONMENT=='LOCAL':
+	BASE_DIR="./nlp-qna/app"
+else:
+	BASE_DIR=""
 
 app.mount("/static", StaticFiles(directory=str(Path(BASE_DIR, "static"))), name="static")
 
-deg get_templates():
-return Jinja2Templates(directory= str(Path(BASE_DIR, "templates")))
+def get_templates():
+	return Jinja2Templates(directory= str(Path(BASE_DIR, "templates")))
 
 templates= get_templates()
 @pass_context
 def https_url_for(context:dict, name:str, **path_params) -> str:
 	request= context["request"]
-	https_url= request.url_for(name, **path_params)
-	return str(http_url).replace("http", "https")
+	http_url= request.url_for(name, **path_params)
+	return str(http_url).replace("http", "https",1)
 
 
 if ENVIRONMENT!='LOCAL':
@@ -62,20 +82,20 @@ async def index(request: Request, user=Depends(fetch_user)):
 	names= [i for i in names if allowed_file(i)]
 	return templates.TemplateResponse("index.html", context= {"request":request, "file_names":names})
 
+
 @app.post("/", response_class=HTMLResponse, dependencies=[Depends(get_templates)])
 async def upload_files_web(request:Request, collection_var= None, user=Depends(fetch_user), files: List(UploadFile)= File(...)):
 	_- process_uploaded_files(files= files, collection=collection_var, user_id= str(user.id))
-	names= get_redis_keys(collection=None, user_id= ste(user.id))
+	names= get_redis_keys(collection=None, user_id= str(user.id))
 	names= [i for i in names if allowed_file(i)]
 	return templates.TemplateResponse("index.html", context= {"request":request, "file_names":names})
 
 @app.get("/reset/web", response_class=HTMLResponse, dependencies=[Depends(get_templates)])
 async def reset_web(request:Request, user= Depends(fetch_user)):
 	_= delete_files(collection=None, user_id= str(user.id))
-	names= get_redis_keys(collection=None, user_id= ste(user.id))
+	names= get_redis_keys(collection=None, user_id= str(user.id))
 	names= [i for i in names if allowed_file(i)]
 	return templates.TemplateResponse("index.html", context= {"request":request, "file_names":names})
-
 
 
 
@@ -87,16 +107,39 @@ def load_qna():
 
 
 
-model_path=""
+if ENVIRONMENT=='LOCAL':
+	model_path=r"D:\model_dump\Bert-qa\model"
+else:
+	model_path=r"D:\model_dump\Bert-qa\model"
 
 
 
 @app.post("/predict/web", response_class=HTMLResponse, dependencies=[Depends(get_templates)])
 async def predict_web(request:Request, search: str=Form(...), user= Depends(fetch_user)):
 	load_qna()
-	response_class=get_final_response(qna, question= search, collection=None, user_id=str(user.id))
+	responses=get_final_response(qna, question= search, collection=None, user_id=str(user.id))
 	context= {"request":request, "responses": responses['results'], "search_query": search}
 	return templates.TemplateResponse("search.html", context=context)
+
+
+################################################
+
+@app.middleware("http")
+async def llog_requests(request:Request, call_next):
+	idem="".join(random.choices(string.ascii_uppercase+string.digits,k=6))
+	logger.info(f"rid={idem} start request path={request.url.path}")
+	start_time= time.time
+
+	response= await call_next(request)
+
+	process_time=(time.time() -start_time)*1000
+	formatted_process_time='{0:2f}'.forrmat(process_time)
+	logger.info(f'rrid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}')
+	return response
+
+
+	
+
 
 
 
